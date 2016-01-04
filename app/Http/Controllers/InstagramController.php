2f4f4ba084d48bc8b18c8b7793007a634016dfea
Yuta Ohashi <ooohhhaaassshhhiii@gmail.com>
2016-01-04 17:16:24 +0900

    Fix bug for instagram controller

--- app/Http/Controllers/InstagramController.php
+++ app/Http/Controllers/InstagramController.php
@@ -14,8 +14,8 @@ class InstagramController extends Controller
         $data = $request->all();
 
         $count      = isset($data['count'])      ? $data['count'] : 20;
-        $max_tag_id = isset($data['max_tag_id']) ? $data['max_tag_id'] : 20;
-        $min_tag_id = isset($data['min_tag_id']) ? $data['min_tag_id'] : 20;
+        $max_tag_id = isset($data['max_tag_id']) ? $data['max_tag_id'] : false;
+        $min_tag_id = isset($data['min_tag_id']) ? $data['min_tag_id'] : false;
 
         $instagram = new Instagram();
         $json = $instagram->callTagsMediaRecentAPI('浜松まつり', $count, $max_tag_id, $min_tag_id);
